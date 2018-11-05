6df9deba440e1add2f5a25c9cab4a1d502089206
keeperlee <leejamesjian@163.com>
2018-11-05 20:47:35 +0800

    通用侧边导航开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,9 +2,13 @@
 * @Author: ASUS
 * @Date:   2018-11-03 10:19:38
 * @Last Modified by:   ASUS
-* @Last Modified time: 2018-11-05 12:26:06
+* @Last Modified time: 2018-11-05 20:20:37
 */
 
 'use strict';
+require('page/common/nav/index.js');
 require('page/common/header/index.js');
+var navSide = require('page/common/nav-side/index.js');
 var _mm = require('util/mm.js');
+
+navSide.init('user-center');
