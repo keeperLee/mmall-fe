75725e812ec0600e06af304b2e2f973f8549f0e9
keeperlee <leejamesjian@163.com>
2018-11-05 13:10:59 +0800

    通用header开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,9 +2,9 @@
 * @Author: ASUS
 * @Date:   2018-11-03 10:19:38
 * @Last Modified by:   ASUS
-* @Last Modified time: 2018-11-04 23:31:45
+* @Last Modified time: 2018-11-05 12:26:06
 */
 
 'use strict';
-require('page/common/nav/index.js');
+require('page/common/header/index.js');
 var _mm = require('util/mm.js');
